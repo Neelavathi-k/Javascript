d7df6801c02d62e59a18a35b64adf8b0748b059a
Neelavathi K27 <neelak27@in.ibm.com>
2022-10-15 13:27:13 +0530

    kk

---
+++ TypeScript01/app.js
@@ -0,0 +1,15 @@
+function add(n1, n2, showResult, resultPhrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var showResult = true;
+var resultPhrase = "Result is : ";
+var result = add(number1, number2, showResult, resultPhrase);
+console.log(result);
